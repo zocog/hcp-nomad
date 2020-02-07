73e3a05da0d5b84f9ed8cc7ac8bc6ae0dbe2bb06
Drew Bailey <2614075+drewbailey@users.noreply.github.com>
2020-02-07 09:38:33 -0500

    move ent only test out of shared oss test file

---
+++ nomad/server_test_ent.go
@@ -0,0 +1,122 @@
+// +build ent
+
+package nomad
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/consul/lib/freeport"
+	"github.com/hashicorp/net-rpc-msgpackrpc"
+	"github.com/hashicorp/nomad/command/agent/consul"
+	"github.com/hashicorp/nomad/helper/testlog"
+	"github.com/hashicorp/nomad/nomad/mock"
+	"github.com/hashicorp/nomad/nomad/structs"
+)
+
+func testACLServer(t *testing.T, cb func(*Config)) (*Server, *structs.ACLToken) {
+	server := testServer(t, func(c *Config) {
+		c.ACLEnabled = true
+		if cb != nil {
+			cb(c)
+		}
+	})
+	token := mock.ACLManagementToken()
+	err := server.State().BootstrapACLTokens(1, 0, token)
+	if err != nil {
+		t.Fatalf("failed to bootstrap ACL token: %v", err)
+	}
+	return server, token
+}
+
+func testServer(t *testing.T, cb func(*Config)) *Server {
+	// Setup the default settings
+	config := DefaultConfig()
+	config.Build = "0.8.0+unittest"
+	config.DevMode = true
+	nodeNum := atomic.AddUint32(&nodeNumber, 1)
+	config.NodeName = fmt.Sprintf("nomad-%03d", nodeNum)
+
+	// Tighten the Serf timing
+	config.SerfConfig.MemberlistConfig.BindAddr = "127.0.0.1"
+	config.SerfConfig.MemberlistConfig.SuspicionMult = 2
+	config.SerfConfig.MemberlistConfig.RetransmitMult = 2
+	config.SerfConfig.MemberlistConfig.ProbeTimeout = 50 * time.Millisecond
+	config.SerfConfig.MemberlistConfig.ProbeInterval = 100 * time.Millisecond
+	config.SerfConfig.MemberlistConfig.GossipInterval = 100 * time.Millisecond
+
+	// Tighten the Raft timing
+	config.RaftConfig.LeaderLeaseTimeout = 50 * time.Millisecond
+	config.RaftConfig.HeartbeatTimeout = 50 * time.Millisecond
+	config.RaftConfig.ElectionTimeout = 50 * time.Millisecond
+	config.RaftTimeout = 500 * time.Millisecond
+
+	// Tighten the autopilot timing
+	config.AutopilotConfig.ServerStabilizationTime = 100 * time.Millisecond
+	config.AutopilotConfig.LastContactThreshold = 200 * time.Millisecond
+	config.ServerHealthInterval = 50 * time.Millisecond
+	config.AutopilotInterval = 100 * time.Millisecond
+
+	// Disable Vault
+	f := false
+	config.VaultConfig.Enabled = &f
+
+	// Squelch output when -v isn't specified
+	if !testing.Verbose() {
+		config.LogOutput = ioutil.Discard
+	}
+
+	// Invoke the callback if any
+	if cb != nil {
+		cb(config)
+	}
+
+	// Enable raft as leader if we have bootstrap on
+	config.RaftConfig.StartAsLeader = !config.DevDisableBootstrap
+	config.Logger = testlog.HCLogger(t)
+	catalog := consul.NewMockCatalog(config.Logger)
+	acls := consul.NewMockACLsAPI(config.Logger)
+
+	for i := 10; i >= 0; i-- {
+		// Get random ports
+		ports := freeport.GetT(t, 2)
+		config.RPCAddr = &net.TCPAddr{
+			IP:   []byte{127, 0, 0, 1},
+			Port: ports[0],
+		}
+		config.SerfConfig.MemberlistConfig.BindPort = ports[1]
+
+		// Create server
+		server, err := NewServer(config, catalog, acls)
+		if err == nil {
+			return server
+		} else if i == 0 {
+			t.Fatalf("err: %v", err)
+		} else {
+			if server != nil {
+				server.Shutdown()
+			}
+			wait := time.Duration(rand.Int31n(2000)) * time.Millisecond
+			time.Sleep(wait)
+		}
+	}
+
+	return nil
+}
+
+func testJoin(t *testing.T, s1 *Server, other ...*Server) {
+	addr := fmt.Sprintf("127.0.0.1:%d",
+		s1.config.SerfConfig.MemberlistConfig.BindPort)
+	for _, s2 := range other {
+		if num, err := s2.Join([]string{addr}); err != nil {
+			t.Fatalf("err: %v", err)
+		} else if num != 1 {
+			t.Fatalf("bad: %d", num)
+		}
+	}
+}
