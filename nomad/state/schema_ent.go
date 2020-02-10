0c572fb8bb78fe6bf5c2e872d57dcb46816f1a50
Drew Bailey <2614075+drewbailey@users.noreply.github.com>
2020-02-10 10:39:01 -0500

    fsm ent clean up, search endpoints for ent

--- nomad/state/schema_ent.go
+++ nomad/state/schema_ent.go
@@ -16,7 +16,33 @@ func init() {
 		sentinelPolicyTableSchema,
 		quotaSpecTableSchema,
 		quotaUsageTableSchema,
+		namespaceTableSchema,
 	}...)
+}
+
+// namespaceTableSchema returns the MemDB schema for the namespace table.
+func namespaceTableSchema() *memdb.TableSchema {
+	return &memdb.TableSchema{
+		Name: TableNamespaces,
+		Indexes: map[string]*memdb.IndexSchema{
+			"id": {
+				Name:         "id",
+				AllowMissing: false,
+				Unique:       true,
+				Indexer: &memdb.StringFieldIndex{
+					Field: "Name",
+				},
+			},
+			"quota": {
+				Name:         "quota",
+				AllowMissing: true,
+				Unique:       false,
+				Indexer: &memdb.StringFieldIndex{
+					Field: "Quota",
+				},
+			},
+		},
+	}
 }
 
 // sentinelPolicyTableSchema returns the MemDB schema for the sentinel policy
