a1c74d2b485e7065b11d5943fe0e8a2d378c112f
Drew Bailey <2614075+drewbailey@users.noreply.github.com>
2020-03-27 09:45:03 -0400

    rename state to not overwrite pkg import

--- nomad/csi_endpoint_ent_test.go
+++ nomad/csi_endpoint_ent_test.go
@@ -9,6 +9,7 @@ import (
 	msgpackrpc "github.com/hashicorp/net-rpc-msgpackrpc"
 	"github.com/hashicorp/nomad/acl"
 	"github.com/hashicorp/nomad/nomad/mock"
+	"github.com/hashicorp/nomad/nomad/state"
 	"github.com/hashicorp/nomad/nomad/structs"
 	"github.com/hashicorp/nomad/testutil"
 	"github.com/stretchr/testify/require"
@@ -24,25 +25,25 @@ func TestCSIPluginEndpoint_ACLNamespaceAlloc(t *testing.T) {
 	})
 	defer shutdown()
 	testutil.WaitForLeader(t, srv.RPC)
-	state := srv.fsm.State()
+	s := srv.fsm.State()
 
 	ns1 := mock.Namespace()
-	require.NoError(t, state.UpsertNamespaces(1000, []*structs.Namespace{ns1}))
+	require.NoError(t, s.UpsertNamespaces(1000, []*structs.Namespace{ns1}))
 
 	// Setup ACLs
-	state.BootstrapACLTokens(1, 0, mock.ACLManagementToken())
+	s.BootstrapACLTokens(1, 0, mock.ACLManagementToken())
 	srv.config.ACLEnabled = true
 	codec := rpcClient(t, srv)
 	listJob := mock.NamespacePolicy(structs.DefaultNamespace, "", []string{acl.NamespaceCapabilityReadJob})
 	policy := mock.PluginPolicy("read") + listJob
-	getToken := mock.CreatePolicyAndToken(t, state, 1001, "plugin-read", policy)
+	getToken := mock.CreatePolicyAndToken(t, s, 1001, "plugin-read", policy)
 
 	// Create the plugin and then some allocations to pretend to be the allocs that are
 	// running the plugin tasks
 	deleteNodes := state.CreateTestCSIPlugin(srv.fsm.State(), "foo")
 	defer deleteNodes()
 
-	plug, _ := state.CSIPluginByID(memdb.NewWatchSet(), "foo")
+	plug, _ := s.CSIPluginByID(memdb.NewWatchSet(), "foo")
 	var allocs []*structs.Allocation
 	for _, info := range plug.Controllers {
 		a := mock.Alloc()
@@ -58,7 +59,7 @@ func TestCSIPluginEndpoint_ACLNamespaceAlloc(t *testing.T) {
 	require.Equal(t, 3, len(allocs))
 	allocs[0].Namespace = ns1.Name
 
-	err := state.UpsertAllocs(1003, allocs)
+	err := s.UpsertAllocs(1003, allocs)
 	require.NoError(t, err)
 
 	req := &structs.CSIPluginGetRequest{
@@ -78,7 +79,7 @@ func TestCSIPluginEndpoint_ACLNamespaceAlloc(t *testing.T) {
 	}
 
 	p2 := mock.PluginPolicy("read")
-	t2 := mock.CreatePolicyAndToken(t, state, 1004, "plugin-read2", p2)
+	t2 := mock.CreatePolicyAndToken(t, s, 1004, "plugin-read2", p2)
 	req.AuthToken = t2.SecretID
 	err = msgpackrpc.CallWithCodec(codec, "CSIPlugin.Get", req, resp)
 	require.NoError(t, err)
