ea471e153fa96af1b05cd12c9e7bd35b83f78aca
Drew Bailey <2614075+drewbailey@users.noreply.github.com>
2020-04-02 09:14:08 -0400

    stick with ent/pro for version info for 0.11

--- version/version_pro.go
+++ version/version_pro.go
@@ -5,5 +5,5 @@ package version
 func init() {
 	// Metadata specifies the type of binary other than the default open-source
 	// version, such as "ent", "pro", etc.
-	VersionMetadata = "platform"
+	VersionMetadata = "pro"
 }
