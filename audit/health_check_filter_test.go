f86713797227d6d2e90221ed3573eb65d22cf141
Drew Bailey <2614075+drewbailey@users.noreply.github.com>
2020-04-02 10:29:08 -0400

    allow partial matching for health check

--- audit/health_check_filter_test.go
+++ audit/health_check_filter_test.go
@@ -13,6 +13,7 @@ import (
 func TestHealthCheckFilter(t *testing.T) {
 	f := &HealthCheckFilter{}
 
+	// Exact match
 	e := testEvent(AuditEvent, OperationReceived)
 	e.Request.Endpoint = "/v1/agent/health"
 
@@ -24,13 +25,25 @@ func TestHealthCheckFilter(t *testing.T) {
 	require.NoError(t, err)
 	require.Nil(t, event)
 
+	// Partial Match
+	e.Request.Endpoint = "/v1/agent/health?type=server"
+
+	payload = eventlogger.Event{
+		Payload: e,
+	}
+
+	event2, err := f.Process(context.Background(), &payload)
+	require.NoError(t, err)
+	require.Nil(t, event2)
+
+	// No match
 	e.Request.Endpoint = "/v1/jobs"
 
 	payload = eventlogger.Event{
 		Payload: e,
 	}
 
-	event, err = f.Process(context.Background(), &payload)
+	event3, err := f.Process(context.Background(), &payload)
 	require.NoError(t, err)
-	require.NotNil(t, event)
+	require.NotNil(t, event3)
 }
