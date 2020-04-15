7c21330479b7bd3c6be16ef0cc21b60b2aff0df3
Aaron Bennett <10927621+abennett@users.noreply.github.com>
2020-04-15 14:27:28 -0500

    add acl test

--- nomad/license_endpoint_test.go
+++ nomad/license_endpoint_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	msgpackrpc "github.com/hashicorp/net-rpc-msgpackrpc"
+	"github.com/hashicorp/nomad/acl"
 	"github.com/hashicorp/nomad/nomad/mock"
 	"github.com/hashicorp/nomad/nomad/structs"
 	"github.com/hashicorp/nomad/testutil"
@@ -20,14 +21,14 @@ func TestLicenseEndpoint_GetLicense(t *testing.T) {
 	testutil.WaitForLeader(t, s1.RPC)
 
 	l := mock.StoredLicense()
-	s1.fsm.State().UpsertLicense(1000, l)
+	require.NoError(t, s1.fsm.State().UpsertLicense(1000, l))
 
 	get := &structs.LicenseGetRequest{
 		QueryOptions: structs.QueryOptions{Region: "global"},
 	}
 	var resp structs.LicenseGetResponse
 	require.NoError(t, msgpackrpc.CallWithCodec(codec, "License.GetLicense", get, &resp))
-	assert.EqualValues(1000, resp.Index)
+	assert.EqualValues(uint64(1000), resp.Index)
 	assert.Equal(l, resp.License)
 }
 
@@ -54,4 +55,66 @@ func TestLicenseEndpoint_UpsertLicense(t *testing.T) {
 	out, err := s1.fsm.State().License(nil)
 	require.NoError(t, err)
 	assert.Equal(out.Signed, l.Signed)
+}
+
+func TestLicenseEndpoint_UpsertLicenses_ACL(t *testing.T) {
+	assert := assert.New(t)
+	t.Parallel()
+	s1, root, cleanupS1 := TestACLServer(t, nil)
+	defer cleanupS1()
+	codec := rpcClient(t, s1)
+	testutil.WaitForLeader(t, s1.RPC)
+
+	l := mock.StoredLicense()
+	state := s1.fsm.State()
+
+	// Create the token
+	invalidToken := mock.CreateToken(t, state, 1003, []string{"test-invalid", acl.PolicyWrite})
+
+	// Create the register request
+	req := &structs.LicenseUpsertRequest{
+		License:      l,
+		WriteRequest: structs.WriteRequest{Region: "global"},
+	}
+
+	// Upsert the license without a token and expect failure
+	{
+		var resp structs.GenericResponse
+		err := msgpackrpc.CallWithCodec(codec, "License.UpsertLicense", req, &resp)
+		assert.NotNil(err)
+		assert.Equal(err.Error(), structs.ErrPermissionDenied.Error())
+
+		// Check we did not create the namespaces
+		out, err := s1.fsm.State().License(nil)
+		require.NoError(t, err)
+		assert.Nil(out)
+	}
+
+	// Try with an invalid token
+	req.AuthToken = invalidToken.SecretID
+	{
+		var resp structs.GenericResponse
+		err := msgpackrpc.CallWithCodec(codec, "License.UpsertLicense", req, &resp)
+		assert.NotNil(err)
+		assert.Equal(err.Error(), structs.ErrPermissionDenied.Error())
+
+		// Check we did not create the namespaces
+		out, err := s1.fsm.State().License(nil)
+		assert.Nil(err)
+		assert.Nil(out)
+
+	}
+
+	// Try with a root token
+	req.AuthToken = root.SecretID
+	{
+		var resp structs.GenericResponse
+		assert.Nil(msgpackrpc.CallWithCodec(codec, "License.UpsertLicense", req, &resp))
+		assert.NotEqual(uint64(0), resp.Index)
+
+		// Check we created the namespaces
+		out, err := s1.fsm.State().License(nil)
+		require.NoError(t, err)
+		assert.NotNil(out)
+	}
 }
