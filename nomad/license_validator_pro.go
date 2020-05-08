ee360880be3c95731bd7bdc8fc21a1a86202d7ff
Drew Bailey <2614075+drewbailey@users.noreply.github.com>
2020-05-08 13:06:09 -0400

    remove pro from licensing

--- nomad/license_validator_pro.go
+++
@@ -1,9 +0,0 @@
-// +build pro
-
-package nomad
-
-import "github.com/hashicorp/go-licensing"
-
-func licenseValidator(lic *licensing.License, licenseStr string) error { return nil }
-
-func TemporaryFlags() map[string]interface{} { return nil }
