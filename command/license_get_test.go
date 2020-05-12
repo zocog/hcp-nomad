2e5b9db2fdab88ef1a6ce8fb8f08f3dbe379abda
Drew Bailey <2614075+drewbailey@users.noreply.github.com>
2020-05-12 13:05:51 -0400

    temp change enterprise license test (#240)

--- command/license_get_test.go
+++ command/license_get_test.go
@@ -20,8 +20,7 @@ func TestCommand_LicenseGet_OSSErr(t *testing.T) {
 
 	code := cmd.Run([]string{"-address=" + url})
 	if srv.Enterprise {
-		require.Equal(t, 0, code)
-		require.Contains(t, ui.OutputWriter.String(), "License Status = valid")
+		require.Equal(t, 1, code)
 	} else {
 		require.Equal(t, 1, code)
 		require.Contains(t, ui.ErrorWriter.String(), "Nomad Enterprise only endpoint")
