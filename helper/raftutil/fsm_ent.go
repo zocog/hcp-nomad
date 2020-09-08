4c55d083c8e9a372c455e59ea6bfcf98980c27fd
Dave May <dmay@hashicorp.com>
2020-09-08 11:19:16 -0400

    Extract license from statestore (#386)
    * Extract license from statestore
    
    * Add empty license array when no license is found
    
    * Verify iter to avoid panic

--- helper/raftutil/fsm_ent.go
+++ helper/raftutil/fsm_ent.go
@@ -9,4 +9,13 @@ func insertEnterpriseState(m map[string][]interface{}, state *state.StateStore) 
 	m["SentinelPolicies"] = toArray(state.SentinelPolicies(nil))
 	m["QuotaSpecs"] = toArray(state.QuotaSpecs(nil))
 	m["QuotaUsages"] = toArray(state.QuotaUsages(nil))
+
+	license, err := state.License(nil)
+	if err != nil {
+		m["License"] = []interface{}{err}
+	} else if license != nil {
+		m["License"] = []interface{}{license}
+	} else {
+		m["License"] = []interface{}{}
+	}
 }
