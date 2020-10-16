4231afa04abf4125c38d8aeb3b3dedc82ea1b391
Buck Doyle <buck@hashicorp.com>
2020-10-16 14:49:45 -0500

    Add namespace wildcard for recommendation fetch (#453)
    This has no test coverage but will gain some when I return
    for the “Include all namespaces” toggle.

--- ui/app/adapters/recommendation-summary.js
+++ ui/app/adapters/recommendation-summary.js
@@ -3,6 +3,11 @@ import ApplicationAdapter from './application';
 export default class RecommendationSummaryAdapter extends ApplicationAdapter {
   pathForType = () => 'recommendations';
 
+  urlForFindAll() {
+    const url = super.urlForFindAll(...arguments);
+    return `${url}?namespace=*`;
+  }
+
   updateRecord(store, type, snapshot) {
     const url = `${super.urlForCreateRecord('recommendations', snapshot)}/apply`;
 
