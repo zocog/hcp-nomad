b996fe3800b1dbfae9b1d6bd5dbd1fb2fc4b071a
Buck Doyle <buck@hashicorp.com>
2020-10-22 15:13:06 -0500

    Fix accordion-hiding to wait for call to proceed

--- ui/app/components/das/recommendation-accordion.js
+++ ui/app/components/das/recommendation-accordion.js
@@ -1,12 +1,26 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { task, timeout } from 'ember-concurrency';
 import ResourcesDiffs from 'nomad-ui/utils/resources-diffs';
 
 export default class DasRecommendationAccordionComponent extends Component {
+  @tracked waitingToProceed = false;
+
   @(task(function*() {
+    this.waitingToProceed = false;
     yield timeout(0);
   }).drop())
   proceed;
+
+  @action
+  inserted() {
+    this.waitingToProceed = true;
+  }
+
+  get show() {
+    return !this.args.summary.isProcessed || this.waitingToProceed;
+  }
 
   get diffs() {
     const summary = this.args.summary;
