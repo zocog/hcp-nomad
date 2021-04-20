3e06bb76721865ad019fa12ee55a4e533b30a3ad
Seth Hoenig <shoenig@hashicorp.com>
2021-04-20 14:23:30 -0600

    e2e: add e2e tests for consul namespaces on ent with acls
    This PR adds e2e tests for Consul Namespaces for Nomad Enterprise
    with Consul ACLs enabled.
    
    Needed to add support for Consul ACL tokens with `namespace` and
    `namespace_prefix` blocks, which Nomad parses and validates before
    tossing the token. These bits will need to be picked back to OSS.

--- e2e/consul/namespaces_ent.go
+++ e2e/consul/namespaces_ent.go
@@ -3,37 +3,63 @@
 package consul
 
 import (
+	"os"
+
 	"github.com/hashicorp/nomad/e2e/framework"
 )
 
+func init() {
+	// Consul Namespace tests with Consul ACLs enabled. Like Connect w/ ACLs tests,
+	// these are gated behind the NOMAD_TEST_CONSUL_ACLS environment variable,
+	// because they cause lots of problems for e2e test flakiness (due to restarting
+	// consul, nomad, etc.).
+	//
+	// Run these tests locally when working on Consul.
+	if os.Getenv("NOMAD_TEST_CONSUL_ACLS") == "1" {
+		framework.AddSuites(&framework.TestSuite{
+			Component:   "ConsulACLs",
+			CanRunLocal: false,
+			Consul:      true,
+			Parallel:    false,
+			Cases: []framework.TestCase{
+				new(ConsulNamespacesE2ETestACLs),
+			},
+		})
+	}
+}
+
+// In these tests, tc.cToken is empty when Consul ACLs are not enabled, and is
+// set to the global-management token when ACLs are enabled. In both cases tests
+// should work, as the management token has complete access to everything.
+
 func (tc *ConsulNamespacesE2ETest) TestConsulRegisterGroupService(f *framework.F) {
-	tc.testConsulRegisterGroupServices(f, "apple", "banana", "cherry", "default")
+	tc.testConsulRegisterGroupServices(f, tc.cToken, "apple", "banana", "cherry", "default")
 }
 
 func (tc *ConsulNamespacesE2ETest) TestConsulRegisterTaskServices(f *framework.F) {
-	tc.testConsulRegisterTaskServices(f, "apple", "banana", "cherry", "default")
+	tc.testConsulRegisterTaskServices(f, tc.cToken, "apple", "banana", "cherry", "default")
 }
 
 func (tc *ConsulNamespacesE2ETest) TestConsulTemplateKV(f *framework.F) {
-	tc.testConsulTemplateKV(f, "value: ns_banana", "value: ns_default")
+	tc.testConsulTemplateKV(f, tc.cToken, "value: ns_banana", "value: ns_default")
 }
 
 func (tc *ConsulNamespacesE2ETest) TestConsulConnectSidecars(f *framework.F) {
-	tc.testConsulConnectSidecars(f, "apple", "default")
+	tc.testConsulConnectSidecars(f, tc.cToken, "apple", "default")
 }
 
 func (tc *ConsulNamespacesE2ETest) TestConsulConnectIngressGateway(f *framework.F) {
-	tc.testConsulConnectIngressGateway(f, "apple", "default")
+	tc.testConsulConnectIngressGateway(f, tc.cToken, "apple", "default")
 }
 
 func (tc *ConsulNamespacesE2ETest) TestConsulConnectTerminatingGateway(f *framework.F) {
-	tc.testConsulConnectTerminatingGateway(f, "apple", "default")
+	tc.testConsulConnectTerminatingGateway(f, tc.cToken, "apple", "default")
 }
 
 func (tc *ConsulNamespacesE2ETest) TestConsulScriptChecksTask(f *framework.F) {
-	tc.testConsulScriptChecksTask(f, "apple", "default")
+	tc.testConsulScriptChecksTask(f, tc.cToken, "apple", "default")
 }
 
 func (tc *ConsulNamespacesE2ETest) TestConsulScriptChecksGroup(f *framework.F) {
-	tc.testConsulScriptChecksGroup(f, "apple", "default")
+	tc.testConsulScriptChecksGroup(f, tc.cToken, "apple", "default")
 }
