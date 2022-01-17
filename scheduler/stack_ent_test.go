ec8f2343a6a120af3ee51e8dcb416aa2f1ad8df1
James Rasell <jrasell@users.noreply.github.com>
2022-01-17 09:33:04 +0100

    quota: fix iterator error when ranked node fails later in stack. (#672)
    The quota iterator is responsible for checking whether a planned
    allocation breaks the resource quota limits it must adhere to.
    
    In the previous behaviour, a call to Next(), would modify the
    iterator stored proposed limit state when calculating whether the
    new alloc would exceed quota. The problem here arises when a node
    passes through the quota iterator, but fails further down the
    stack process. In this scenario, a new node is passed through the
    stack. When this new node reaches the quota iterator, a new
    proposed quota calculation is performed which includes addition
    from both node iterations.
    
    The proposed quota state within the iterator can become inflated
    meaning job plans and registrations fail due to quota violations
    in error. This is only resolved once the allocation has been
    placed on a node and the Reset() iterator function is called.
    
    The change means the iterator always works on a copy of the
    proposed quota change.

--- scheduler/stack_ent_test.go
+++ scheduler/stack_ent_test.go
@@ -5,6 +5,7 @@ package scheduler
 import (
 	"testing"
 
+	"github.com/hashicorp/nomad/helper/uuid"
 	"github.com/hashicorp/nomad/nomad/mock"
 	"github.com/hashicorp/nomad/nomad/structs"
 	"github.com/stretchr/testify/assert"
@@ -202,4 +203,76 @@ func TestQuotaIterator_BelowQuota_DiscountStopping(t *testing.T) {
 	contextual.SetTaskGroup(job.TaskGroups[0])
 	quota.Reset()
 	assert.Len(collectFeasible(quota), 1)
+}
+
+// TestQuotaIterator_BelowQuota_MultipleNextCallsForAlloc ensures the proposed
+// limit of the QuotaIterator is not modified until we have placed an
+// allocation.
+//
+// This specifically tests against a found bug which was caused by the iterator
+// modifying the proposedLimit even if the node was later deemed not suitable
+// for placing the allocation on.
+func TestQuotaIterator_BelowQuota_MultipleNextCallsForAlloc(t *testing.T) {
+	newAssert := assert.New(t)
+	state, ctx := testContext(t)
+
+	// Create a quota spec which has enough resource limit to cover over 50% of
+	// that needed by the mock job. The values at the time of writing are 1.6
+	// times that required.
+	qs := &structs.QuotaSpec{
+		Name:        "quota-spec-" + uuid.Generate(),
+		Description: "Super uncool quota!",
+		Limits: []*structs.QuotaLimit{
+			{
+				Region: "global",
+				RegionLimit: &structs.Resources{
+					CPU:      8000,
+					MemoryMB: 4096,
+				},
+			},
+		},
+	}
+	qs.SetHash()
+
+	newAssert.Nil(state.UpsertQuotaSpecs(100, []*structs.QuotaSpec{qs}))
+
+	// Create the namespace and ensure the quota is linked.
+	ns := mock.Namespace()
+	ns.Quota = qs.Name
+	newAssert.Nil(state.UpsertNamespaces(200, []*structs.Namespace{ns}))
+
+	// Create the job.
+	job := mock.Job()
+	job.Namespace = ns.Name
+
+	// Create the source with three nodes, so we can hit the Next() function
+	// a couple of times.
+	nodes := make([]*structs.Node, 3)
+
+	for i := 0; i < 3; i++ {
+		nodes[i] = mock.Node()
+	}
+	static := NewStaticIterator(ctx, nodes)
+
+	// Create the quota iterator using the struct rather than the factory
+	// method, so we have access to its glorious internals.
+	quota := &QuotaIterator{ctx: ctx, source: static}
+
+	// Perform the calls performed when starting a new placement calculation.
+	quota.SetJob(job)
+	quota.SetTaskGroup(job.TaskGroups[0])
+	quota.Reset()
+
+	// Copy the current proposed limit state. Until the context is updated with
+	// a NodeUpdate or NodeAllocation and Reset is called, this should not
+	// change.
+	currentLimitState := quota.proposedLimit.Copy()
+
+	// Iterate through nodes in the stack, each time checking that the proposed
+	// limit has not changed.
+	newAssert.Equal(currentLimitState, quota.proposedLimit)
+	_ = quota.Next()
+	newAssert.Equal(currentLimitState, quota.proposedLimit)
+	_ = quota.Next()
+	newAssert.Equal(currentLimitState, quota.proposedLimit)
 }
