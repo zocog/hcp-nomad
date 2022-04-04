3fed4d17da0b30786a1495a5704991801af27ce1
Claire Labry <claire@hashicorp.com>
2022-04-04 09:55:12 -0400

    lint up hcl + build files

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,9 +2,9 @@
 
 version_file=$1
 version_metadata_file=$2
-version=$(awk '$1 == "Version" && $2 == "=" { gsub(/"/, "", $3); print $3 }' < "${version_file}")
-prerelease=$(awk '$1 == "VersionPrerelease" && $2 == "=" { gsub(/"/, "", $3); print $3 }' < "${version_file}")
-metadata=$(awk '$1 == "VersionMetadata" && $2 == "=" { gsub(/"/, "", $3); print $3 }' < "${version_metadata_file}")
+version=$(awk '$1 == "Version" && $2 == "=" { gsub(/"/, "", $3); print $3 }' <"${version_file}")
+prerelease=$(awk '$1 == "VersionPrerelease" && $2 == "=" { gsub(/"/, "", $3); print $3 }' <"${version_file}")
+metadata=$(awk '$1 == "VersionMetadata" && $2 == "=" { gsub(/"/, "", $3); print $3 }' <"${version_metadata_file}")
 
 if [ -n "$metadata" ] && [ -n "$prerelease" ]; then
     echo "${version}-${prerelease}+${metadata}"
