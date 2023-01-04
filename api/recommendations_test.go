b3ec552fcf8db7bcd2002085066c84d0268eb665
James Rasell <jrasell@users.noreply.github.com>
2023-01-04 16:49:46 +0100

    api: remove direct use of testify from enterprise code. (#1018)

--- api/recommendations_test.go
+++ api/recommendations_test.go
@@ -7,12 +7,12 @@ import (
 	"testing"
 
 	"github.com/hashicorp/nomad/api/internal/testutil"
-	"github.com/stretchr/testify/require"
+	"github.com/shoenig/test/must"
 )
 
 func TestRecommendations_Upsert(t *testing.T) {
 	testutil.Parallel(t)
-	require := require.New(t)
+
 	c, s := makeClient(t, nil, nil)
 	defer s.Stop()
 	recommendations := c.Recommendations()
@@ -20,29 +20,29 @@ func TestRecommendations_Upsert(t *testing.T) {
 	// Create a recommendation and register it
 	ns := testNamespace()
 	_, err := c.Namespaces().Register(ns, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	job := testJob()
 	job.Namespace = pointerOf(ns.Name)
 	_, _, err = c.Jobs().Register(job, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec := testRecommendation(job)
 	rec, wm, err := recommendations.Upsert(rec, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	assertWriteMeta(t, wm)
 
 	// Query the recommendations back out again
 	resp, qm, err := recommendations.List(&QueryOptions{
 		Namespace: ns.Name,
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 1)
-	require.Equal(rec.ID, resp[0].ID)
+	must.Len(t, 1, resp)
+	must.Eq(t, rec.ID, resp[0].ID)
 }
 
 func TestRecommendations_Upsert_Invalid(t *testing.T) {
 	testutil.Parallel(t)
-	require := require.New(t)
+
 	c, s := makeClient(t, nil, nil)
 	defer s.Stop()
 	recommendations := c.Recommendations()
@@ -50,21 +50,21 @@ func TestRecommendations_Upsert_Invalid(t *testing.T) {
 	// Invalid resource
 	ns := testNamespace()
 	_, err := c.Namespaces().Register(ns, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	job := testJob()
 	job.Namespace = pointerOf(ns.Name)
 	_, _, err = c.Jobs().Register(job, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec := testRecommendation(job)
 	rec.Resource = "Wrong"
 	ret, _, err := recommendations.Upsert(rec, nil)
-	require.Error(err)
-	require.Nil(ret)
+	must.Error(t, err)
+	must.Nil(t, ret)
 }
 
 func TestRecommendation_Info(t *testing.T) {
 	testutil.Parallel(t)
-	require := require.New(t)
+
 	c, s := makeClient(t, nil, nil)
 	defer s.Stop()
 	recommendations := c.Recommendations()
@@ -72,28 +72,28 @@ func TestRecommendation_Info(t *testing.T) {
 	// Create a recommendation and register it
 	ns := testNamespace()
 	_, err := c.Namespaces().Register(ns, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	job := testJob()
 	job.Namespace = pointerOf(ns.Name)
 	_, _, err = c.Jobs().Register(job, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec := testRecommendation(job)
 	rec, wm, err := recommendations.Upsert(rec, nil)
-	require.NoError(err)
-	require.NotNil(rec)
+	must.NoError(t, err)
+	must.NotNil(t, rec)
 	assertWriteMeta(t, wm)
 
 	// Query the recommendation and compare
 	info, qm, err := recommendations.Info(rec.ID, nil)
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.NotNil(info)
-	require.Equal(rec, info)
+	must.NotNil(t, info)
+	must.Eq(t, rec, info)
 }
 
 func TestRecommendations_Delete(t *testing.T) {
 	testutil.Parallel(t)
-	require := require.New(t)
+
 	c, s := makeClient(t, nil, nil)
 	defer s.Stop()
 	recommendations := c.Recommendations()
@@ -101,32 +101,32 @@ func TestRecommendations_Delete(t *testing.T) {
 	// Create a recommendation and register it
 	ns := testNamespace()
 	_, err := c.Namespaces().Register(ns, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	job := testJob()
 	job.Namespace = pointerOf(ns.Name)
 	_, _, err = c.Jobs().Register(job, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec := testRecommendation(job)
 	rec, wm, err := recommendations.Upsert(rec, nil)
-	require.NoError(err)
-	require.NotNil(rec)
+	must.NoError(t, err)
+	must.NotNil(t, rec)
 	assertWriteMeta(t, wm)
 
 	// Delete the recommendation
 	wm, err = recommendations.Delete([]string{rec.ID}, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	assertWriteMeta(t, wm)
 
 	list, _, err := recommendations.List(&QueryOptions{
 		Namespace: ns.Name,
 	})
-	require.NoError(err)
-	require.Len(list, 0)
+	must.NoError(t, err)
+	must.Len(t, 0, list)
 }
 
 func TestRecommendations_List(t *testing.T) {
 	testutil.Parallel(t)
-	require := require.New(t)
+
 	c, s := makeClient(t, nil, nil)
 	defer s.Stop()
 	recommendations := c.Recommendations()
@@ -135,43 +135,43 @@ func TestRecommendations_List(t *testing.T) {
 	resp, qm, err := recommendations.List(&QueryOptions{
 		Namespace: "*",
 	})
-	require.Nil(err)
-	require.Empty(resp)
+	must.NoError(t, err)
+	must.Len(t, 0, resp)
 
 	// Create recommendations in two namespaces
 	ns1 := testNamespace()
 	ns1.Name = generateUUID()
 	_, err = c.Namespaces().Register(ns1, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	job1 := testJob()
 	job1.Namespace = pointerOf(ns1.Name)
 	_, _, err = c.Jobs().Register(job1, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec1 := testRecommendation(job1)
 	rec1, wm, err := recommendations.Upsert(rec1, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	assertWriteMeta(t, wm)
 
 	ns2 := testNamespace()
 	ns2.Name = generateUUID()
 	_, err = c.Namespaces().Register(ns2, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	job2 := testJob()
 	job2.Namespace = pointerOf(ns2.Name)
 	_, _, err = c.Jobs().Register(job2, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec2 := testRecommendation(job2)
 	rec2, wm, err = recommendations.Upsert(rec2, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	assertWriteMeta(t, wm)
 
 	// Query all recommendations, all namespaces
 	resp, qm, err = recommendations.List(&QueryOptions{
 		Namespace: "*",
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 2)
+	must.Len(t, 2, resp)
 
 	// Query the recommendations using a non-matching prefix
 	prefix := generateUUID()
@@ -184,42 +184,42 @@ func TestRecommendations_List(t *testing.T) {
 	resp, qm, err = recommendations.List(&QueryOptions{
 		Prefix: prefix[0:2],
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 0)
+	must.Len(t, 0, resp)
 
 	// Query the recommendations using a matching prefix
 	resp, qm, err = recommendations.List(&QueryOptions{
 		Namespace: ns1.Name,
 		Prefix:    rec1.ID[0:2],
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 1)
-	require.Equal(rec1.ID, resp[0].ID)
+	must.Len(t, 1, resp)
+	must.Eq(t, rec1.ID, resp[0].ID)
 
 	// Query the recommendations using a matching prefix, wrong namespace
 	resp, qm, err = recommendations.List(&QueryOptions{
 		Namespace: "wrong",
 		Prefix:    rec1.ID[0:2],
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 0)
+	must.Len(t, 0, resp)
 
 	// Query the recommendations by namespace
 	resp, qm, err = recommendations.List(&QueryOptions{
 		Namespace: ns1.Name,
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 1)
-	require.Equal(resp[0].ID, rec1.ID)
+	must.Len(t, 1, resp)
+	must.Eq(t, resp[0].ID, rec1.ID)
 }
 
 func TestRecommendations_ListFilter(t *testing.T) {
 	testutil.Parallel(t)
-	require := require.New(t)
+
 	c, s := makeClient(t, nil, nil)
 	defer s.Stop()
 	recommendations := c.Recommendations()
@@ -228,38 +228,38 @@ func TestRecommendations_ListFilter(t *testing.T) {
 	resp, qm, err := recommendations.List(&QueryOptions{
 		Namespace: "*",
 	})
-	require.Nil(err)
-	require.Empty(resp)
+	must.Nil(t, err)
+	must.Len(t, 0, resp)
 
 	// Create recommendations in two namespaces
 	ns := testNamespace()
 	ns.Name = generateUUID()
 	_, err = c.Namespaces().Register(ns, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	job1 := testJob()
 	job1.Namespace = pointerOf(ns.Name)
 	_, _, err = c.Jobs().Register(job1, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec1 := testRecommendation(job1)
 	rec1, _, err = recommendations.Upsert(rec1, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	job2 := testJob()
 	job2.ID = pointerOf(generateUUID())
 	job2.Namespace = pointerOf(ns.Name)
 	_, _, err = c.Jobs().Register(job2, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec2 := testRecommendation(job2)
 	_, _, err = recommendations.Upsert(rec2, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 
 	// By namespace
 	resp, qm, err = recommendations.List(&QueryOptions{
 		Namespace: ns.Name,
 		Params:    map[string]string{},
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 2)
+	must.Len(t, 2, resp)
 
 	// By job, present
 	resp, qm, err = recommendations.List(&QueryOptions{
@@ -268,10 +268,10 @@ func TestRecommendations_ListFilter(t *testing.T) {
 			"job": *job1.ID,
 		},
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 1)
-	require.Equal(rec1.ID, resp[0].ID)
+	must.Len(t, 1, resp)
+	must.Eq(t, rec1.ID, resp[0].ID)
 
 	// By job, absent
 	resp, qm, err = recommendations.List(&QueryOptions{
@@ -280,9 +280,9 @@ func TestRecommendations_ListFilter(t *testing.T) {
 			"job": "wrong",
 		},
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 0)
+	must.Len(t, 0, resp)
 
 	// Task group
 	resp, qm, err = recommendations.List(&QueryOptions{
@@ -292,10 +292,10 @@ func TestRecommendations_ListFilter(t *testing.T) {
 			"group": *job1.TaskGroups[0].Name,
 		},
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 1)
-	require.Equal(rec1.ID, resp[0].ID)
+	must.Len(t, 1, resp)
+	must.Eq(t, rec1.ID, resp[0].ID)
 
 	// Task group, absent
 	resp, qm, err = recommendations.List(&QueryOptions{
@@ -305,9 +305,9 @@ func TestRecommendations_ListFilter(t *testing.T) {
 			"group": "wrong",
 		},
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 0)
+	must.Len(t, 0, resp)
 
 	// Task
 	resp, qm, err = recommendations.List(&QueryOptions{
@@ -318,10 +318,10 @@ func TestRecommendations_ListFilter(t *testing.T) {
 			"task":  job1.TaskGroups[0].Tasks[0].Name,
 		},
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 1)
-	require.Equal(rec1.ID, resp[0].ID)
+	must.Len(t, 1, resp)
+	must.Eq(t, rec1.ID, resp[0].ID)
 
 	// Task, absent
 	resp, qm, err = recommendations.List(&QueryOptions{
@@ -332,14 +332,14 @@ func TestRecommendations_ListFilter(t *testing.T) {
 			"task":  "wrong",
 		},
 	})
-	require.Nil(err)
+	must.Nil(t, err)
 	assertQueryMeta(t, qm)
-	require.Len(resp, 0)
+	must.Len(t, 0, resp)
 }
 
 func TestRecommendations_Apply(t *testing.T) {
 	testutil.Parallel(t)
-	require := require.New(t)
+
 	c, s := makeClient(t, nil, nil)
 	defer s.Stop()
 	recommendations := c.Recommendations()
@@ -348,72 +348,72 @@ func TestRecommendations_Apply(t *testing.T) {
 	resp, _, err := recommendations.List(&QueryOptions{
 		Namespace: "*",
 	})
-	require.Nil(err)
-	require.Empty(resp)
+	must.NoError(t, err)
+	must.Len(t, 0, resp)
 
 	// Create recommendations in two namespaces
 	ns := testNamespace()
 	ns.Name = generateUUID()
 	_, err = c.Namespaces().Register(ns, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	job1 := testJob()
 	job1.Namespace = pointerOf(ns.Name)
 	_, _, err = c.Jobs().Register(job1, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec1cpu := testRecommendation(job1)
 	rec1cpu, _, err = recommendations.Upsert(rec1cpu, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec1mem := testRecommendation(job1)
 	rec1mem.Resource = "MemoryMB"
 	rec1mem.Value = *job1.TaskGroups[0].Tasks[0].Resources.MemoryMB * 2
 	rec1mem, _, err = recommendations.Upsert(rec1mem, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	job2 := testJob()
 	job2.ID = pointerOf(generateUUID())
 	job2.Namespace = pointerOf(ns.Name)
 	_, _, err = c.Jobs().Register(job2, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec2cpu := testRecommendation(job2)
 	rec2cpu, _, err = recommendations.Upsert(rec2cpu, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 	rec2mem := testRecommendation(job2)
 	rec2mem.Resource = "MemoryMB"
 	rec2mem.Value = *job2.TaskGroups[0].Tasks[0].Resources.MemoryMB * 2
 	rec2mem, _, err = recommendations.Upsert(rec2mem, nil)
-	require.NoError(err)
+	must.NoError(t, err)
 
 	// Recs are present
 	resp, _, err = recommendations.List(&QueryOptions{
 		Namespace: "*",
 	})
-	require.Nil(err)
-	require.Len(resp, 4)
+	must.NoError(t, err)
+	must.Len(t, 4, resp)
 
 	// Apply recs
 	applyResponse, wm, err := recommendations.Apply(
 		[]string{rec1cpu.ID, rec2cpu.ID, rec2mem.ID},
 		false)
-	require.NoError(err)
-	require.Empty(applyResponse.Errors)
-	require.Len(applyResponse.UpdatedJobs, 2)
+	must.NoError(t, err)
+	must.Len(t, 0, applyResponse.Errors)
+	must.Len(t, 2, applyResponse.UpdatedJobs)
 	assertWriteMeta(t, &applyResponse.WriteMeta)
 	assertWriteMeta(t, wm)
 
 	// Jobs are updated
 	job1, _, _ = c.Jobs().Info(*job1.ID, &QueryOptions{Namespace: *job1.Namespace})
-	require.Greater(*job1.JobModifyIndex, *job1.CreateIndex)
-	require.Equal(rec1cpu.Value, *job1.TaskGroups[0].Tasks[0].Resources.CPU)
-	require.Equal(256, *job1.TaskGroups[0].Tasks[0].Resources.MemoryMB)
+	must.Greater(t, *job1.CreateIndex, *job1.JobModifyIndex)
+	must.Eq(t, rec1cpu.Value, *job1.TaskGroups[0].Tasks[0].Resources.CPU)
+	must.Eq(t, 256, *job1.TaskGroups[0].Tasks[0].Resources.MemoryMB)
 	job2, _, _ = c.Jobs().Info(*job2.ID, &QueryOptions{Namespace: *job2.Namespace})
-	require.Greater(*job2.JobModifyIndex, *job2.CreateIndex)
-	require.Equal(rec2cpu.Value, *job2.TaskGroups[0].Tasks[0].Resources.CPU)
-	require.Equal(rec2mem.Value, *job2.TaskGroups[0].Tasks[0].Resources.MemoryMB)
+	must.Greater(t, *job2.CreateIndex, *job2.JobModifyIndex)
+	must.Eq(t, rec2cpu.Value, *job2.TaskGroups[0].Tasks[0].Resources.CPU)
+	must.Eq(t, rec2mem.Value, *job2.TaskGroups[0].Tasks[0].Resources.MemoryMB)
 
 	// Applied recs are gone
 	resp, _, err = recommendations.List(&QueryOptions{
 		Namespace: "*",
 	})
-	require.Nil(err)
-	require.Len(resp, 1)
-	require.Equal(rec1mem.ID, resp[0].ID)
+	must.NoError(t, err)
+	must.Len(t, 1, resp)
+	must.Eq(t, rec1mem.ID, resp[0].ID)
 }
