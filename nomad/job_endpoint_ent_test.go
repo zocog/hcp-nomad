502ab29cad7151156d6a878f5f14f56000f97bf6
Luiz Aoqui <luiz@hashicorp.com>
2023-05-01 17:57:58 -0400

    Revert "deps: update go-msgpack to v2.0.0 in ENT files"
    This reverts commit c9eef56471986f8bbd105772597515d85445c6ab.

--- nomad/job_endpoint_ent_test.go
+++ nomad/job_endpoint_ent_test.go
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	"github.com/hashicorp/go-memdb"
-	msgpackrpc "github.com/hashicorp/net-rpc-msgpackrpc/v2"
+	msgpackrpc "github.com/hashicorp/net-rpc-msgpackrpc"
 	"github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/nomad/ci"
 	"github.com/hashicorp/nomad/command/agent/consul"
