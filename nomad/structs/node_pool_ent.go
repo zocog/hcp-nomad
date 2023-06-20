52c88691f0e867c5755e208c413c2c9aae172ca6
Luiz Aoqui <luiz@hashicorp.com>
2023-06-20 10:02:33 -0400

    ci: add temporary ENT implementation

---
+++ nomad/structs/node_pool_ent.go
@@ -0,0 +1,10 @@
+//go:build ent
+// +build ent
+
+package structs
+
+// Validate returns an error if the node pool scheduler confinguration is
+// invalid.
+func (n *NodePoolSchedulerConfiguration) Validate() error {
+	return nil
+}
